8ff3a97af4da3af5df3b6e00520913546c0e53fd
Rebecca Dettmar <rmdettmar@gmail.com>
2024-06-27 21:52:15 +0000

    flake.nix cleanup

--- l3x/flake.nix
+++ l3x/flake.nix
@@ -4,9 +4,6 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils"; # Utility functions for Nix flakes
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Main Nix package repository
-    # localSrc = {
-    #     url = "path:./l3x";
-    # };
   };
 
   outputs = { self, flake-utils, nixpkgs, ... }:
@@ -28,49 +25,15 @@
           sourceRoot = "${src.name}/l3x";
 
           nativeBuildInputs = with pkgs; [
-            cacert
             openssl
             openssl.dev
             pkg-config
           ];
           buildInputs = with pkgs; [
-            cacert
             openssl
             openssl.dev
             pkg-config
           ];
-
-          pathsToLink = [
-            "/bin"
-            "/etc/ssl/certs"
-          ];
-
-          RUSTFLAGS = [
-            "-C" "link-arg=-L${pkgs.openssl.dev}/lib"
-            "-C" "link-arg=-lssl"
-            "-C" "link-arg=-lcrypto"
-          ];
-          
-          shellHook = ''
-            echo "PREBUILD"
-            export SSL_CERT_FILE="/etc/ssl/certs/ca-bundle.crt"
-            export SSL_CERT_DIR="/etc/ssl/certs"
-            export CARGO_HTTP_CAINFO="/etc/ssl/certs/ca-bundle.crt"
-            export OPENSSL_DIR="${pkgs.openssl.dev}"
-            export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
-            export OPENSSL_LIBS="${pkgs.openssl.dev}/lib"
-          '';
-
-          # buildPhase = ''
-          #   # echo ${src.name}
-          #   echo "REBECCA"
-          #   # echo "aAAAa"
-          #   # if [! -f Cargo.lock ]; then
-          #   #   echo "Cargo.lock not found"
-          #   #   cargo generate-lockfile
-          #   # fi
-          #   # cargo build --release
-          # '';
         };
       }
     );
