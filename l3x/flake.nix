e9cc110f4f3f5c11307966388d02f0ddfdc92a3c
David Shepard <daveman1010220@gmail.com>
2025-05-19 14:32:26 -0400

    update the rust toolchain

--- l3x/flake.nix
+++ l3x/flake.nix
@@ -4,14 +4,25 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils"; # Utility functions for Nix flakes
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Main Nix package repository
+
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, ... }:
+  outputs = { self, flake-utils, nixpkgs, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-      };
+          config = {
+            cc = "clang";
+          };
+          overlays = [ 
+            rust-overlay.overlays.default
+          ];
+        };
+
+        rustToolChain = pkgs.rust-bin.nightly.latest.default;
       in {
         packages.default = pkgs.rustPlatform.buildRustPackage rec {
           pname = "l3x";
@@ -23,7 +34,11 @@
           cargoHash = "sha256-tfL33xSZjV9iN40sIK2XQXlF/jSixmUFJRv7V20MGVg=";
           sourceRoot = "${src.name}/l3x";
 
+          RUSTC = "${rustToolChain}/bin/rustc";
+          CARGO = "${rustToolChain}/bin/cargo";
+
           nativeBuildInputs = with pkgs; [
+            rustToolChain
             openssl
             openssl.dev
             pkg-config
