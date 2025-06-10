4c57ef4143e81438ddddb001d9b88855439c6edc
David Shepard <daveman1010220@gmail.com>
2025-06-10 13:53:22 -0400

    Attempt to fix the build error

--- l3x/flake.nix
+++ l3x/flake.nix
@@ -2,14 +2,14 @@
   description = "l3x flake";
 
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils"; # Utility functions for Nix flakes
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Main Nix package repository
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, flake-utils, nixpkgs, rust-overlay, ... }:
+  outputs = { flake-utils, nixpkgs, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -24,15 +24,17 @@
 
         rustToolChain = pkgs.rust-bin.nightly.latest.default;
       in {
-        packages.default = pkgs.rustPlatform.buildRustPackage rec {
+          packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "l3x";
-          version = "2";
-          src = pkgs.fetchgit {
-            url = "https://github.com/daveman1010221/l3x";
-            hash = "sha256-xiOu9GGdvWtFX04w2Rk0ozRw2LO2HE0TQ7akH7J3d7Y=";
+          version = "0.1.0";
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
           };
-          cargoHash = "sha256-tfL33xSZjV9iN40sIK2XQXlF/jSixmUFJRv7V20MGVg=";
-          sourceRoot = "${src.name}/l3x";
+
+          sourceRoot = "l3x";
 
           RUSTC = "${rustToolChain}/bin/rustc";
           CARGO = "${rustToolChain}/bin/cargo";
